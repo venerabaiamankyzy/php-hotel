121a025d91cd4da698a47c143333dd66b54c3655
Venera Baiaman kyzy <venerabayaman@gmail.com>
2023-03-12 21:06:46 +0100

    added hotels with all data.

--- index.php
+++ index.php
@@ -10,6 +10,58 @@ Bonus
 2 - Aggiungere un secondo campo al form che permetta di filtrare gli hotel per voto (es. inserisco 3 ed ottengo tutti gli hotel che hanno un voto di tre stelle o superiore)
 NOTA: deve essere possibile utilizzare entrambi i filtri contemporaneamente (es. ottenere una lista con hotel che dispongono di parcheggio e che hanno un voto di tre stelle o superiore)
 Se non viene specificato nessun filtro, visualizzare come in precedenza tutti gli hotel. -->
+<?php
+
+    $hotels = [
+
+        [
+            'name' => 'Hotel Belvedere',
+            'description' => 'Hotel Belvedere Descrizione',
+            'parking' => true,
+            'vote' => 4,
+            'distance_to_center' => 10.4
+        ],
+        [
+            'name' => 'Hotel Futuro',
+            'description' => 'Hotel Futuro Descrizione',
+            'parking' => true,
+            'vote' => 2,
+            'distance_to_center' => 2
+        ],
+        [
+            'name' => 'Hotel Rivamare',
+            'description' => 'Hotel Rivamare Descrizione',
+            'parking' => false,
+            'vote' => 1,
+            'distance_to_center' => 1
+        ],
+        [
+            'name' => 'Hotel Bellavista',
+            'description' => 'Hotel Bellavista Descrizione',
+            'parking' => false,
+            'vote' => 5,
+            'distance_to_center' => 5.5
+        ],
+        [
+            'name' => 'Hotel Milano',
+            'description' => 'Hotel Milano Descrizione',
+            'parking' => true,
+            'vote' => 2,
+            'distance_to_center' => 50
+        ],
+
+    ];
+
+    // foreach($hotels as $allhotels) {
+    //   // var_dump($allhotels);
+
+    //   foreach($allhotels as $hotel){
+    //     var_dump($hotel);
+    //   }
+      
+    // }
+
+?>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -20,6 +72,13 @@ Se non viene specificato nessun filtro, visualizzare come in precedenza tutti gl
   <title>PHP Hotel</title>
 </head>
 <body>
-  
+  <ul>
+    <?php foreach($hotels as $allhotels) { ?>
+      <?php foreach($allhotels as $hotel) { ?>
+        <li><?= $hotel ?> </li>
+      <?php } ?>
+    <?php } ?> 
+  </ul>
+
 </body>
 </html>
